0bf77d2f62acda909cd44d1c8a90305d2c3fad95
ShwethaUmashanker <shwthshanker@gmail.com>
2019-04-15 22:41:14 -0400

    trying link to login

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const http = require('http');
 const port=process.env.PORT || 1414
-const server = http.createServer((req, res) => {
+/*const server = http.createServer((req, res) => {
 	res.statusCode = 200;
 	res.setHeader('Content-Type', 'text/html');
 	res.end('<h1>Hello World</h1>');
-	});
-server.listen(port,() => {
+	});*/
+var express = require('express')
+var app = express()
+
+// respond with "hello world" when a GET request is made to the homepage
+app.get('/', function (req, res) {
+  res.send('hello world')
+})
+app.listen(port,() => {
 	console.log(`Server running at port `+port);
 	});
